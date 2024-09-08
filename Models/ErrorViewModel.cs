343cdfe0eb834f1338e979a3e06ba0e455b7b237
Kaan EFE <kaanefe9796@gmail.com>
2024-09-08 16:39:58 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace week7day2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
